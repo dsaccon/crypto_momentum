a273dcff912f145e8db946718cf03cf5e1aa602e
ds <deerff@gmail.com>
2020-12-23 15:38:35 -0800

    allow strat to trade on every tick in the shorter time period

--- strategies/willr_bband.py
+++ strategies/willr_bband.py
@@ -17,24 +17,30 @@ class WillRBband(BacktestingBaseClass):
             'short_entry_cross': ('close', 'bband_20_high'),
             'short_close_cross': ('close', 'bband_20_low'),
         }
-        #self.data.append(pd.DataFrame()) # Add new cols to DataFrame in last list element
 
     def preprocess_data(self):
         super().preprocess_data()
 
-        # ..load 60m data (index=1)
+        # ..load 60m data
         i = 1
         self.data[i]['willr'] = btalib.willr(self.data[i]['high'], self.data[i]['low'], self.data[i]['close'], period = 14).df
         self.data[i]['willr_ema'] = btalib.ema(self.data[i]['willr'], period = 43, _seed = 3).df
         self.data[i]['willr_ema_prev'] = self.data[i]['willr_ema'].shift(1)
 
-        # ..load 3m data (index=0)
+        # ..load 3m data
         i = 0
         self.data[i]['bband_20_low'] = btalib.bbands(self.data[i]['close'], period = 20, devs = 2.3).bot
         self.data[i]['bband_20_low_prev'] = self.data[i]['bband_20_low'].shift(1)
         self.data[i]['bband_20_high'] = btalib.bbands(self.data[i]['close'], period = 20, devs = 2.3).top
         self.data[i]['bband_20_high_prev'] = self.data[i]['bband_20_high'].shift(1)
         self.data[i]['close_prev'] = self.data[i]['close'].shift(1)
+
+        # Upsample 60m data to dataframe at index=0
+        modulo = int(self.cfg['series'][1][-1])
+        for _i, row in self.data[0].iterrows():
+            dt_60m = int(_i - _i % modulo)
+            self.data[0].at[_i, 'willr_ema'] = self.data[1].at[dt_60m, 'willr_ema']
+            self.data[0].at[_i, 'willr_ema_prev'] = self.data[1].at[dt_60m, 'willr_ema_prev']
 
         i = 0
         # For Long entry
@@ -49,25 +55,25 @@ class WillRBband(BacktestingBaseClass):
         # For Short close
         self.get_crosses('close', 'bband_20_low', i, over=False)
 
-    def _execute_trade(self, row_0, row_1): 
-        if self.position == 0 and row_1['willr_ema'].iloc[0] > row_1['willr_ema_prev'].iloc[0]:
-            if row_0[self.cross_buy_open_col]:
+    def _execute_trade(self, row):
+        if self.position == 0 and row['willr_ema'] > row['willr_ema_prev']:
+            if row[self.cross_buy_open_col]:
                 # Long entry
-                self.trades.append(('Buy', 'Open', row_0['close']))
+                self.trades.append(('Buy', 'Open', row['close']))
                 self.position = 1
-        elif self.position > 0 and row_0[self.cross_buy_close_col]:
+        elif self.position > 0 and row[self.cross_buy_close_col]:
                 # Long close
-                self.trades.append(('Buy', 'Close', row_0['close']))
+                self.trades.append(('Buy', 'Close', row['close']))
                 self.position = 0
                 return True
-        elif self.position == 0 and row_1['willr_ema'].iloc[0] < row_1['willr_ema_prev'].iloc[0]:
-            if row_0[self.cross_sell_open_col]:
+        elif self.position == 0 and row['willr_ema'] < row['willr_ema_prev']:
+            if row[self.cross_sell_open_col]:
                 # Short entry
-                self.trades.append(('Sell', 'Open', row_0['close']))
+                self.trades.append(('Sell', 'Open', row['close']))
                 self.position = -1
-        elif self.position < 0 and row_0[self.cross_sell_close_col]:
+        elif self.position < 0 and row[self.cross_sell_close_col]:
                 # Short close
-                self.trades.append(('Sell', 'Close', row_0['close']))
+                self.trades.append(('Sell', 'Close', row['close']))
                 self.position = 0
                 return True
         return False
@@ -78,23 +84,22 @@ class WillRBband(BacktestingBaseClass):
         if not self._crosses_sanity_check():
             raise SanityCheckError
         processed_rows = 0
-        for i_3m, row_0 in self.data[0].iterrows(): # iterate over 3m series
+        for i, row in self.data[0].iterrows(): # iterate over 3m series
             #if i_3m % 20 == 0:
             #row_1 = self.data[1].iloc[int(i_3m/20)] # corresponding 60m row
-            row_1 = (self.data[1].loc[self.data[0]['datetime'] == row_0.datetime])
-            if row_1.empty:
-                # End of 60m series
-                break
-            if any([
-                    row_1.isna().willr_ema.iloc[0],
-                    row_1.isna().willr_ema_prev.iloc[0]]):
-                # Skip first row before 60m' first candle
-                continue
-            processed_rows += 1
-            close = self._execute_trade(row_0, row_1)
-            if close:
+#            row_1 = (self.data[1].loc[self.data[0]['datetime'] == row_0.datetime])
+#            if row_1.empty:
+#                # End of 60m series
+#                break
+#            if any([
+#                    row_1.isna().willr_ema.iloc[0],
+#                    row_1.isna().willr_ema_prev.iloc[0]]):
+#                # Skip first row before 60m' first candle
+#                continue
+#            processed_rows += 1
+            if self._execute_trade(row)
                 # If a position was closed in this candle, check to re-open new position
-                self._execute_trade(row_0, row_1)
+                self._execute_trade(row)
 
 #            # Only progress 60m series iteration when ts' line up
 #            if self.position == 0 and row_1['willr_ema'].iloc[0] > row_1['willr_ema_prev'].iloc[0]:
