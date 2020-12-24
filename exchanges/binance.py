4dd3a8501ea9015db497cae7854cc144fc5a8527
ds <deerff@gmail.com>
2020-12-24 15:22:22 -0800

    remove commented out base class, moved to __init__()

--- exchanges/binance.py
+++ exchanges/binance.py
@@ -45,17 +45,6 @@ class BinanceRequestException(Exception):
         return 'BinanceRequestException: %s' % self.message
 
 
-#$class ExchangeAPI:
-#$    name = ''
-#$
-#$    def __init__(self):
-#$        self.base_uri = ''
-#$        self.max_candles_fetch = None
-#$        self.trading_fee = None
-#$        self.KEY = None
-#$        self.SECRET = None
-#$
-
 class BinanceAPI(ExchangeAPI):
     name = 'binance'
 
