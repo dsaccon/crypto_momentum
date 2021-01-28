02dc05e77de61723249dd3aec628bd1114f4d112
ds <>
2021-01-28 18:58:20 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+OS=`uname`
+
+if [ "$OS" == 'Darwin' ]; then
+    brew update
+    brew cask install ta-lib
+elif [ "$OS" == 'Linux' ]; then
+    wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+    tar xvzf ta-lib-0.4.0-src.tar.gz
+    cd ta-lib
+    ./configure --prefix=/usr
+    make
+    sudo make install
+    cd ..
+    rm ta-lib-0.4.0-src.tar.gz
+fi
+
+pipenv install
