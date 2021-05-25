2e5ffec68377260a2f1f27f98c086cada06890cf
David <david.g.saccon@gmail.com>
2021-05-25 07:28:03 +0000

    add extra checks so it can be run in Dockerfile

--- install.sh
+++ install.sh
@@ -6,17 +6,27 @@ if [ "$OS" == 'Darwin' ]; then
     brew update
     brew cask install ta-lib
 elif [ "$OS" == 'Linux' ]; then
-    sudo apt update
-    sudo apt install -y pipenv
+    USER=`whoami`
+    if [ "$USER" == 'root' ]; then
+        PREFIX=''
+    else
+        PREFIX='sudo '
+    fi
+    $PREFIX apt update
+    $PREFIX apt install -y pipenv
     wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
     tar xvzf ta-lib-0.4.0-src.tar.gz
     cd ta-lib
     ./configure --prefix=/usr
     make
-    sudo make install
+    $PREFIX make install
     cd ..
     rm ta-lib-0.4.0-src.tar.gz
     rm -r ta-lib
 fi
 
-pipenv install
+if [ "$1" == --no-pipenv-install ]; then
+    : # do nothing
+else
+    pipenv install
+fi
