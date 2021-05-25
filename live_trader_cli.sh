a6d55db67d3b5f3a6af54a45e83228a5a3d81844
ds <david.g.saccon@gmail.com>
2021-05-25 19:01:36 +0000

    add logic in --start to check if container already exists

--- live_trader_cli.sh
+++ live_trader_cli.sh
@@ -5,11 +5,22 @@
 if [ "$1" == --start ]; then
 	ctr_name=ta_trader_$2
     if [ "$3" == --build ]; then
+        sudo docker stop $ctr_name
+        sudo docker rm $ctr_name
         sudo AWS_SNS_KEY=$AWS_SNS_KEY AWS_SNS_SECRET=$AWS_SNS_SECRET \
             BINANCE_KEY=$BINANCE_KEY BINANCE_SECRET=$BINANCE_SECRET \
             docker-compose up -d --build $ctr_name
     else
-        sudo docker-compose start $ctr_name
+	    str=$(sudo docker ps -a --filter "name=$ctr_name" | grep -v CREATED)
+        if [[ "$str" == *"$ctr_name"* ]]; then
+            # Container already exists. Just start it up
+            sudo docker-compose start $ctr_name
+        else
+            # Container does not exist already. Bring a new one up
+            sudo AWS_SNS_KEY=$AWS_SNS_KEY AWS_SNS_SECRET=$AWS_SNS_SECRET \
+                BINANCE_KEY=$BINANCE_KEY BINANCE_SECRET=$BINANCE_SECRET \
+                docker-compose up -d $ctr_name
+        fi
     fi
 elif [ "$1" == --stop ]; then
 	ctr_name=ta_trader_$2
