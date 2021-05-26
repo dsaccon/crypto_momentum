c55e260ca8ee1d7cb75032253deeab5a30828cc8
ds <david.g.saccon@gmail.com>
2021-05-26 22:53:29 +0000

    change to start/stop containers logic

--- live_trader_cli.sh
+++ live_trader_cli.sh
@@ -4,30 +4,52 @@
 
 if [ "$1" == --start ]; then
 	ctr_name=ta_trader_$2
-    if [ "$3" == --build ]; then
-	    str=$(sudo docker ps --filter "name=$ctr_name" | grep -v CREATED)
-        if [[ "$str" == *"$ctr_name"* ]]; then
-            sudo docker stop $ctr_name
-        fi
-	    str=$(sudo docker ps -a --filter "name=$ctr_name" | grep -v CREATED)
-        if [[ "$str" == *"$ctr_name"* ]]; then
-            sudo docker rm $ctr_name
-        fi
-        sudo AWS_SNS_KEY=$AWS_SNS_KEY AWS_SNS_SECRET=$AWS_SNS_SECRET \
-            BINANCE_KEY=$BINANCE_KEY BINANCE_SECRET=$BINANCE_SECRET \
-            docker-compose up -d --build $ctr_name
-    else
-	    str=$(sudo docker ps -a --filter "name=$ctr_name" | grep -v CREATED)
-        if [[ "$str" == *"$ctr_name"* ]]; then
-            # Container already exists. Just start it up
-            sudo docker-compose start $ctr_name
-        else
-            # Container does not exist already. Bring a new one up
-            sudo AWS_SNS_KEY=$AWS_SNS_KEY AWS_SNS_SECRET=$AWS_SNS_SECRET \
-                BINANCE_KEY=$BINANCE_KEY BINANCE_SECRET=$BINANCE_SECRET \
-                docker-compose up -d $ctr_name
-        fi
+    str=$(sudo docker ps --filter "name=$ctr_name" | grep -v CREATED)
+    if [[ "$str" == *"$ctr_name"* ]]; then
+        # Stop container if its already running
+        sudo docker stop $ctr_name
     fi
+    str=$(sudo docker ps -a --filter "name=$ctr_name" | grep -v CREATED)
+    if [[ "$str" == *"$ctr_name"* ]]; then
+        # Remove container if it already exists
+        sudo docker rm $ctr_name
+    fi
+    if [ "$3" == --build ]; then
+	    #str=$(sudo docker ps --filter "name=$ctr_name" | grep -v CREATED)
+        #if [[ "$str" == *"$ctr_name"* ]]; then
+        #    sudo docker stop $ctr_name
+        #fi
+	    #str=$(sudo docker ps -a --filter "name=$ctr_name" | grep -v CREATED)
+        #if [[ "$str" == *"$ctr_name"* ]]; then
+        #    sudo docker rm $ctr_name
+        #fi
+        suffix='--build '$ctr_name
+        #sudo AWS_SNS_KEY=$AWS_SNS_KEY AWS_SNS_SECRET=$AWS_SNS_SECRET \
+        #    BINANCE_KEY=$BINANCE_KEY BINANCE_SECRET=$BINANCE_SECRET \
+        #    docker-compose up -d --build $ctr_name
+    else
+	    #str=$(sudo docker ps --filter "name=$ctr_name" | grep -v CREATED)
+        #if [[ "$str" == *"$ctr_name"* ]]; then
+        #    sudo docker stop $ctr_name
+        #fi
+	    #str=$(sudo docker ps -a --filter "name=$ctr_name" | grep -v CREATED)
+        #if [[ "$str" == *"$ctr_name"* ]]; then
+        #    sudo docker rm $ctr_name
+        #fi
+        #else
+        #    # Container does not exist already. Bring a new one up
+        #    sudo AWS_SNS_KEY=$AWS_SNS_KEY AWS_SNS_SECRET=$AWS_SNS_SECRET \
+        #        BINANCE_KEY=$BINANCE_KEY BINANCE_SECRET=$BINANCE_SECRET \
+        #        docker-compose up -d $ctr_name
+        #fi
+        suffix=$ctr_name
+        #sudo AWS_SNS_KEY=$AWS_SNS_KEY AWS_SNS_SECRET=$AWS_SNS_SECRET \
+        #    BINANCE_KEY=$BINANCE_KEY BINANCE_SECRET=$BINANCE_SECRET \
+        #    docker-compose up -d $ctr_name
+    fi
+    sudo AWS_SNS_KEY=$AWS_SNS_KEY AWS_SNS_SECRET=$AWS_SNS_SECRET \
+        BINANCE_KEY=$BINANCE_KEY BINANCE_SECRET=$BINANCE_SECRET \
+        docker-compose up -d $suffix
 elif [ "$1" == --stop ]; then
 	ctr_name=ta_trader_$2
 	sudo docker-compose stop $ctr_name
