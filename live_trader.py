ffcfb6e79df97d95aaa4ecb864cfa4c25dbb4951
ds <david.g.saccon@gmail.com>
2021-05-28 04:39:42 +0000

    add prefetch period and start using _align_first_row()

--- live_trader.py
+++ live_trader.py
@@ -29,49 +29,62 @@ class LiveTrader(Base):
         for i, _cfg in enumerate(self.data_cfg):
             if _cfg[2]*self.strategy.MAX_PERIODS[i] > interval:
                 interval = _cfg[2]*self.strategy.MAX_PERIODS[i]
+        if not self.data_prefetch_period > interval:
+            raise DataConfigurationError
+
         _end = dt.datetime.utcnow()
         self.end = (_end.year, _end.month, _end.day, _end.hour, _end.minute)
         self.end_ts = _end.timestamp()
-        days_padding = 10
-        interval += days_padding*86400 + max([c[2] for c in self.data_cfg])
-        self.start_ts = int(dt.datetime.utcnow().timestamp()) - interval
+#        self.start_ts = int(dt.datetime.utcnow().timestamp()) - self.data_prefetch_period
+        self.start_prefetch = dt.datetime.utcnow() - dt.timedelta(seconds=self.data_prefetch_period)
 
-        longest_period = max([_[2] for _ in self.data_cfg])
-        shortest_period = min([_[2] for _ in self.data_cfg])
-        if not longest_period % shortest_period == 0:
-            raise DataConfigurationError
-
-        while True:
-            # Push back start ts until it lines up with both series' periods
-            if self.start_ts % longest_period == 0:
-                break
-            else:
-                self.start_ts -= 1
-        self.start = dt.datetime.fromtimestamp(self.start_ts)
-        self.start = (
-            self.start.year,
-            self.start.month,
-            self.start.day,
-            self.start.hour,
-            self.start.minute)
+        self._align_first_row()
+#        longest_period = max([_[2] for _ in self.data_cfg])
+#        shortest_period = min([_[2] for _ in self.data_cfg])
+#        if not longest_period % shortest_period == 0:
+#            raise DataConfigurationError
+#
+#        while True:
+#            # Push back start ts until it lines up with both series' periods
+#            if self.start_ts % longest_period == 0:
+#                break
+#            else:
+#                self.start_ts -= 1
+#        self.start = dt.datetime.fromtimestamp(self.start_ts)
+#        self.start = (
+#            self.start.year,
+#            self.start.month,
+#            self.start.day,
+#            self.start.hour,
+#            self.start.minute)
         self.exchange_obj = self.exchange_cls(use_testnet=args.use_testnet)
 
-    def _align_first_row(self):
-        """
-        For more than one series, make sure ts of first aligns with each period
-        Otherwise, this will mess up some of the series calculations
-        Basically, make sure first row ts had mod 0 for each period 
-        """
-        quit() 
-        longest_period = max([_[2] for _ in self.data_cfg])
-        logging.info(self.data_cfg)
-        logging.info(longest_period)
-        while True:
-            if all([
-                    d.iloc[0].index % longest_period == 0
-                    for d in self.data
-                    ]):
-                return
+#    def _align_first_row(self):
+#        """
+#
+#        Make sure starting row aligns between both the longer and shorter series
+#
+#        """
+#
+#        longest_period = max([_[2] for _ in self.data_cfg])
+#        shortest_period = min([_[2] for _ in self.data_cfg])
+#        if not longest_period % shortest_period == 0:
+#            raise DataConfigurationError
+#        
+#        start_ts = int(self.start_prefetch.timestamp())
+#        while True:
+#            # Push back start ts until it lines up with both series' periods
+#            if start_ts % longest_period == 0:
+#                break
+#            else:
+#                start_ts -= 1
+#        start = dt.datetime.fromtimestamp(start_ts)
+#        self.start_prefetch = (
+#            start.year,
+#            start.month,
+#            start.day,
+#            start.hour,
+#            start.minute)
 
     def get_data(self):
         # Fetch data from exchange
@@ -106,7 +119,6 @@ class LiveTrader(Base):
             data = self.df
         else:
             raise DataCollectionError
-
         self.strategy(self.df, self.exchange_obj, self.trading_cfg).run()
 
 
