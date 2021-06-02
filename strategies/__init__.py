12b6e3eb4bf51b7191d1a00aaec544da316b803f
ds <david.g.saccon@gmail.com>
2021-06-02 04:28:03 +0000

    add evo

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -7,6 +7,7 @@ import btalib
 
 from .willr_ema import WillREma
 from .willr_bband import WillRBband, LiveWillRBband
+from .willr_bband_evo import WillRBbandEvo, LiveWillRBbandEvo
 from .willr_bband_cross_mod import WillRBbandCrossMod
 from .ha import HA
 from .ema_lrc import EmaLrc
