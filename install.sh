aaa5ddcd82b3eb086443d825dddd01e020384a52
ds <david.g.saccon@gmail.com>
2021-06-04 18:00:52 +0000

    add py3.9 and clean up other platforms

--- install.sh
+++ install.sh
@@ -3,23 +3,29 @@
 OS=`uname`
 
 if [ "$OS" == 'Darwin' ]; then
-    brew update
-    brew cask install ta-lib
+    echo 'Mac not supported. Can only run on Linux'
+    exit 0
 elif [ "$OS" == 'Linux' ]; then
     USER=`whoami`
     if [ "$USER" == 'root' ]; then
-        PREFIX=''
-    else
-        PREFIX='sudo '
+        echo 'Stopping script. For root user, do install manually'
+        exit 0
     fi
-    $PREFIX apt update
-    $PREFIX apt install -y pipenv
+    sudo apt update
+    sudo apt install -y pipenv
+
+    sudo apt install -y software-properties-common
+    sudo add-apt-repository ppa:deadsnakes/ppa
+    sudo apt install -y python3.9
+    sudo apt install python3.9-dev libpq-dev
+
+    # Install ta-lib dependency
     wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
     tar xvzf ta-lib-0.4.0-src.tar.gz
     cd ta-lib
     ./configure --prefix=/usr
     make
-    $PREFIX make install
+    sudo make install
     cd ..
     rm ta-lib-0.4.0-src.tar.gz
     rm -r ta-lib
