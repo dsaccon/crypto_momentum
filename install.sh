5211f98737cc01ab94ce6781f5dcb2c6b95e56a8
Ubuntu <ubuntu@ip-172-31-8-212.us-west-2.compute.internal>
2021-06-16 17:04:43 +0000

    add docker to install

--- install.sh
+++ install.sh
@@ -37,6 +37,15 @@ elif [ "$OS" == 'Linux' ]; then
         sudo apt install -y python3.9
         sudo apt install python3.9-dev libpq-dev
         install_talib 'sudo'
+
+        # Docker
+        sudo apt-get install -y curl apt-transport-https ca-certificates software-properties-common
+
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+        sudo apt-get update
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
     fi
 fi
 
